60ed1d712daca1d3f68a69f06f62a20738d14110
Александр <aleksandr@MacBook-Pro-Aleksandr.local>
2020-03-25 20:13:33 +0200

    Updates

---
+++ precache-manifest.ca54c4db79e586033cb67b9d806a681d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "641c8b0e96e2212dddfcc56c1b9fd859",
+    "url": "/gallery/index.html"
+  },
+  {
+    "revision": "8f8441baa815fc44bd03",
+    "url": "/gallery/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "106d54e266c497d6f096",
+    "url": "/gallery/static/css/main.9ea225d6.chunk.css"
+  },
+  {
+    "revision": "8f8441baa815fc44bd03",
+    "url": "/gallery/static/js/2.543603cc.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/gallery/static/js/2.543603cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "106d54e266c497d6f096",
+    "url": "/gallery/static/js/main.431447ae.chunk.js"
+  },
+  {
+    "revision": "8823c8f372c66259867f",
+    "url": "/gallery/static/js/runtime-main.7bd5114d.js"
+  }
+]);
