49f0f34700eccad816fa934112d3207f96404619
Александр <aleksandr@MacBook-Pro-Aleksandr.local>
2020-03-30 19:22:10 +0300

    Updates

--- precache-manifest.56f1b31fbd605cba1d7f29c7e3983bc3.js
+++ precache-manifest.56f1b31fbd605cba1d7f29c7e3983bc3.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8c04ecf843b0ebe10fdcb43cd4e42390",
+    "revision": "fddb87430ae25cd9ab3fd175cb93575d",
     "url": "/gallery/index.html"
   },
   {
-    "revision": "af6ab46036f03cb85de4",
+    "revision": "4d95ecf7f7df331146d1",
     "url": "/gallery/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "c2ce1338b7c6a029cedc",
+    "revision": "ef33f093408d8e951e98",
     "url": "/gallery/static/css/main.4d26bc86.chunk.css"
   },
   {
-    "revision": "af6ab46036f03cb85de4",
-    "url": "/gallery/static/js/2.76dc36a9.chunk.js"
+    "revision": "4d95ecf7f7df331146d1",
+    "url": "/gallery/static/js/2.ddfc80e9.chunk.js"
   },
   {
     "revision": "94b29ac6d0ec426932e82da99c88b81c",
-    "url": "/gallery/static/js/2.76dc36a9.chunk.js.LICENSE.txt"
+    "url": "/gallery/static/js/2.ddfc80e9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c2ce1338b7c6a029cedc",
-    "url": "/gallery/static/js/main.420db123.chunk.js"
+    "revision": "ef33f093408d8e951e98",
+    "url": "/gallery/static/js/main.47829409.chunk.js"
   },
   {
     "revision": "8823c8f372c66259867f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gallery/precache-manifest.334f514b95e4d43174653b9dd18847ac.js"
+  "/gallery/precache-manifest.56f1b31fbd605cba1d7f29c7e3983bc3.js"
 );
 
 self.addEventListener('message', (event) => {
